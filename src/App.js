01a9193d91ed8e85515b307a65c952702e22a371
unknown <mtarek7878@gmail.com>
2022-03-17 23:46:14 +0200

    complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Cart from './comp/Cart';
+import Header from './comp/Header';
+import Home from './comp/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+     <Header/>
+
+     <Routes>
+     <Route path='/' element ={<Home/>} />
+     <Route path='/cart' element={<Cart/>}/ >
+
+
+
+     </Routes>
+
+     {/* <div>
+       <Route path='/' exact >
+           <Home/>
+       </Route>
+       <Route path='/cart' exact >
+           <Cart/>
+       </Route>
+     </div> */}
+
+    </BrowserRouter>
   );
 }
 
---
+++ src/comp/context/Context.js
@@ -0,0 +1,48 @@
+// import React from 'react'
+import { createContext, useContext, useReducer } from 'react'
+import { cartReducer ,productReducer } from "./Reducers";
+
+
+import faker from 'faker'
+
+const Cart = createContext();
+
+const Context = ({children}) => {
+
+  const products = [...Array(20)].map(() => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.random.image(),
+    inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
+    fastDelivery: faker.datatype.boolean(),
+    ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
+  }));
+   
+
+  const [state,dispatch]= useReducer(cartReducer , {
+    products : products,
+    cart: []
+  })
+
+  const [productState,productDispatch] = useReducer(productReducer , {
+    byStock:false,
+    byFastDelivery:false,
+    byRating:0,
+    searchQuery:"",
+  })
+
+  console.log(products)
+  return (
+    <Cart.Provider value={{state,dispatch,productState,productDispatch}} >
+      {children}
+    </Cart.Provider>
+  )
+}
+
+export default Context
+
+export const CartState = ()=>{
+
+  return useContext (Cart)
+}
